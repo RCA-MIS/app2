666bf6dc02c494e44d95ee815fbc7e0273a287ea
mugishayves <mugishayves189000@gmail.com>
2023-08-08 12:29:30 +0200

    [Feature] Created the admin pages

--- src/components/Achievement/Achievement.tsx
+++ src/components/Achievement/Achievement.tsx
@@ -8,9 +8,8 @@ interface Props {
 }
 const Achievement = ({ date, title, content, image }: Props) => {
         return (
-                <div className="w-full h-[260px] my-4 flex justify-between items-center border border-[#ccc] py-2 px-1 rounded-lg">
+                <div className="w-full h-[100px] my-4 flex justify-between items-center border border-[#ccc] py-2 px-1 rounded-lg">
                         <div className="w-2/5 h-[90%] rounded-lg bg-slate-100">
-                                <Image src={image} alt={title} className="w-full h-full" />
                         </div>
                         <div className="w-3/5 flex flex-col mx-3">
                                 <h4 className="pb-4 text-[#434343ba] text-[10px]">{date}</h4>

e91c030b226ec044dcf58c0e39d4b2fcc3b3330c
mugishayves <mugishayves189000@gmail.com>
2023-08-08 12:24:43 +0200

    [Feature] Created the admin pages

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,4 +1,7 @@
+import Achievement from '@/components/Achievement/Achievement'
 import React from 'react'
+import newOne from "../../../assets/newOne.jpg"
+import newTwo from "../../../assets/newTwo.jpg"
 
 const AdminDashboard = () => {
         return (
@@ -8,9 +11,24 @@ const AdminDashboard = () => {
                                 <p className='text-[rgba(67,67,67,0.43)] my-2'>Statistics of  Rwanda Coding Academy</p>
                         </div>
                         <div className='w-[30%]  h-full bg-[#D9D9D975] border-2 border-[#43434305] p-2 rounded-md'>
-                                <div className="flex flex-row justify-between">
-                                        <p className='text-[rgba(67,67,67,0.43)] my-2'>Achievements</p>
-                                        <div className="h-10 w-10 rounded-full bg-[#687CE2] text-white flex justify-center items-center">82</div>
+                                <div>
+                                        <div className="flex flex-row justify-between">
+                                                <p className='text-[rgba(67,67,67,0.43)] my-2'>Achievements</p>
+                                                <div className="h-10 w-10 rounded-full bg-[#687CE2] text-white flex justify-center items-center">82</div>
+                                        </div>
+                                        <div>
+                                                <Achievement image={newOne} date={"Sunday 24th September, 2020"} title={"World Bank Fund RCA tomorrow"} content={"Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, cons, adipisci . . ."} />
+                                                <Achievement image={newTwo} date={"Sunday 24th September, 2020"} title={"World Bank Fund RCA tomorrow"} content={"Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, cons, adipisci . . ."} />
+                                        </div>
+                                </div>
+                                <div>
+                                        <p className='text-[rgba(0,0,0,0.43)] my-2 text-[17px]'>Publish School Anouncements</p>
+                                        <p className='text-[rgba(67,67,67,0.43)] my-2'>These are only visible to the students who use the school platform</p>
+                                        <form>
+                                                <input type="text" placeholder='Anouncement Heading' className=' px-3 py-2 text-black placeholder:text-black bg-[rgba(67,67,67,0.03)]  rounded-md border-[2px] border-[rgba(67,67,67,0.09)] w-full' />
+                                                <textarea placeholder='Anouncement Body' className="text-black placeholder:text-black bg-[rgba(67,67,67,0.03)]  rounded-md border-[2px] border-[rgba(67,67,67,0.09)] w-full px-3 py-2"></textarea>
+                                                <input type="submit" value="Publish Anouncement"   className='bg-[rgba(82,56,115)] rounded-md text-white px-5 py-2'/>
+                                        </form>
                                 </div>
                         </div>
 
--- src/components/Achievement/Achievement.tsx
+++ src/components/Achievement/Achievement.tsx
@@ -1,9 +1,22 @@
+import Image, { StaticImageData } from 'next/image'
 import React from 'react'
-
-const Achievement = () => {
+interface Props {
+        date: String,
+        title: string,
+        content: String
+        image: StaticImageData
+}
+const Achievement = ({ date, title, content, image }: Props) => {
         return (
-                <div className='w-full flex flex-row gap-2 items-center'>
-                        
+                <div className="w-full h-[260px] my-4 flex justify-between items-center border border-[#ccc] py-2 px-1 rounded-lg">
+                        <div className="w-2/5 h-[90%] rounded-lg bg-slate-100">
+                                <Image src={image} alt={title} className="w-full h-full" />
+                        </div>
+                        <div className="w-3/5 flex flex-col mx-3">
+                                <h4 className="pb-4 text-[#434343ba]">{date}</h4>
+                                <h3 className="font-extrabold pb-4 test-[#49494af0]">{title}</h3>s
+                                <h6 className="text-[#434343ba]">{content}</h6>
+                        </div>
                 </div>
         )
 }

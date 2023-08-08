2667448d78ea6726a8dfa97662dced0a219ed7e1
mugishayves <mugishayves189000@gmail.com>
2023-08-08 11:43:43 +0200

    [Feature] Created the admin pages

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Footer = () => {
         return (
-                <div>Footer</div>
+                <div className='flex flex-row justify-between items-center mx-3  bg-[#D9D9D975] border-2 border-[#43434305] p-1 rounded-md h-[13vh]    w-100%'>
+                </div>
         )
 }
 

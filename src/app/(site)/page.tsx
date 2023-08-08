387f7b4f3c80739bc7910f0d5a21b2cf8ce3c8ad
mugishayves <mugishayves189000@gmail.com>
2023-08-08 12:11:17 +0200

    [Feature] Created the admin pages

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,11 +3,16 @@ import React from 'react'
 const AdminDashboard = () => {
         return (
                 <div className='flex flex-row gap-5'>
-                        <div className='w-[70%]'>
-                        <p>Welcome Back <span>Buringiro Saad</span></p>
-                        <p>Statistics of  Rwanda Coding Academy</p>
+                        <div className='w-[70%] p-3'>
+                                <p className='text-[19px] font-semibold my-2'>Welcome Back <span className='font-extrabold'>Buringiro Saad</span></p>
+                                <p className='text-[rgba(67,67,67,0.43)] my-2'>Statistics of  Rwanda Coding Academy</p>
                         </div>
-                        <div className='w-[30%] bg-[]'></div>
+                        <div className='w-[30%]  h-full bg-[#D9D9D975] border-2 border-[#43434305] p-2 rounded-md'>
+                                <div className="flex flex-row justify-between">
+                                        <p className='text-[rgba(67,67,67,0.43)] my-2'>Achievements</p>
+                                        <div className="h-10 w-10 rounded-full bg-[#687CE2] text-white flex justify-center items-center">82</div>
+                                </div>
+                        </div>
 
                 </div>
         )
---
+++ src/components/Achievement/Achievement.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Achievement = () => {
+        return (
+                <div className='w-full flex flex-row gap-2 items-center'>
+                        
+                </div>
+        )
+}
+
+export default Achievement

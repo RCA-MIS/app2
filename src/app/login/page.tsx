6dbe3abc7670f58cc0d46285fe63dde0d68b1ad6
mugishayves <mugishayves189000@gmail.com>
2023-08-12 16:00:30 +0200

    added content to all pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,7 +10,7 @@ const page = () => {
                         <div className='flex items-center justify-center h-[50%]'>
                                 <div className='rounded-[0.625rem] border border-gray-400 border-opacity-10 px-[2.75rem] py-[1.5rem] text-center max-w-[38%]'>
                                         <h2 className='text-[#00000082]  font-extrabold text-[1.3125rem]'>School Account Login</h2>
-                                        <p className='text-[#4343436E] font-medium text-sm'>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet</p>
+                                        <p className='text-[#4343436E] font-medium text-sm'>Log In to your account</p>
                                         <form>
                                                 <input type="text" className='text-black rounded-md border border-gray-400 border-opacity-9 bg-[#4343430D] w-full px-[1.94rem] py-[1rem] outline-none focus:outline-none my-2.5 placeholder:text-black' placeholder='School Student Code' />
                                                 <input type="password" className='text-black rounded-md border border-gray-400 border-opacity-9 bg-[#4343430D] w-full px-[1.94rem] py-[1rem]  outline-none focus:outline-none my-2.5  placeholder:text-black' placeholder='Password' />
@@ -23,9 +23,7 @@ const page = () => {
                                                 </div>
                                                 <input type="submit" value="Login" className='rounded-full border border-[#2955C56E] border-opacity-43 bg-[#523873] text-white px-[3.5rem]  py-3 my-2.5 text-[1rem] w-[60%]' />
                                         </form>
-                                        <p className='text-[#4343436D] font-medium text-sm text-left'>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, cons
-                                                adipisci velit, sed quia non numquam eius modi ullma tempora incidunt
-                                                ut labore et dolore magnam aliquam.
+                                        <p className='text-[#4343436D] font-medium text-sm text-left'>Rwanda Coding Academy presents a streamlined Student and School Management System for students, parents, and staff. Easily access and oversee attendance, grades, assignments, and communication through our secure login. Real-time updates and notifications keep you informed, making your educational journey at Rwanda Coding Academy efficient and seamless.
                                         </p>
                                         <button className='float-left text-[#52387387] my-1.5'>Read more...</button>
                                 </div>

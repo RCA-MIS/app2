b5a68cfd3096783670577dcccf8afa2be7e0e4da
mugishayves <mugishayves189000@gmail.com>
2023-08-21 10:33:34 +0200

    [Fixed] fixed the customization drag and drop functionality

--- src/components/dropzone/FileDrop.tsx
+++ src/components/dropzone/FileDrop.tsx
@@ -30,20 +30,24 @@ const FileDropZone: FC<{ fileType: string; onFilesSelected: (filetype: string, f
         return (
                 <div {...getRootProps()} className={`dropzone ${isDragActive ? 'active bg-[rgba(42,10,82,0.1)]' : ''}`}>
                         <input {...getInputProps()} />
-                        {selectedImage !== null ? <img src={selectedImage as string} alt="Selected" className="w-full h-full" /> : (
-                                <div>
-                                        {isDragActive ? (
-                                                <div className='bg-[rgba(67,67,67,0.03)] h-[300px] rounded-md border-[2px] border-[rgba(67,67,67,0.09)] flex flex-col gap-5 justify-center items-center'>
-                                                        <p className='text-[rgba(0,0,0,0.1)]  text-[28px] font-semibold'>Drop the file here</p>
-                                                </div>
-                                        ) : (
-                                                <div className='bg-[rgba(67,67,67,0.03)] h-[300px] rounded-md border-[2px] border-[rgba(67,67,67,0.09)] flex flex-col gap-5 justify-center items-center'>
-                                                        <p className='text-[rgba(0,0,0,0.1)]  text-[28px] font-semibold'>Drag & Drop</p>
-                                                        <p className='text-[rgba(73,73,74,0.78)] text-sm font-medium'>{title}</p>
-                                                </div>
-                                        )}
+                        {selectedImage !== null ?
+                                <div className='bg-[rgba(67,67,67,0.03)] h-[300px] rounded-md border-[2px] border-[rgba(67,67,67,0.09)] flex flex-col gap-5 justify-center items-center p-2'>
+                                        <img src={selectedImage as string} alt="Selected" className="w-[90%] h-[90%]" />
                                 </div>
-                        )}
+                                : (
+                                        <div>
+                                                {isDragActive ? (
+                                                        <div className='bg-[rgba(67,67,67,0.03)] h-[300px] rounded-md border-[2px] border-[rgba(67,67,67,0.09)] flex flex-col gap-5 justify-center items-center'>
+                                                                <p className='text-[rgba(0,0,0,0.1)]  text-[28px] font-semibold'>Drop the file here</p>
+                                                        </div>
+                                                ) : (
+                                                        <div className='bg-[rgba(67,67,67,0.03)] h-[300px] rounded-md border-[2px] border-[rgba(67,67,67,0.09)] flex flex-col gap-5 justify-center items-center'>
+                                                                <p className='text-[rgba(0,0,0,0.1)]  text-[28px] font-semibold'>Drag & Drop</p>
+                                                                <p className='text-[rgba(73,73,74,0.78)] text-sm font-medium'>{title}</p>
+                                                        </div>
+                                                )}
+                                        </div>
+                                )}
                 </div>
         );
 };

2d8cac13a79eb206201843bff47f1ae83282080b
MuhireIghor <96773629+MuhireIghor@users.noreply.github.com>
2023-09-14 14:38:03 +0200

    Create index.tsx

---
+++ src/data/index.tsx
@@ -0,0 +1,25 @@
+export interface SwiperInfo {
+    title: string;
+    subTitle: String;
+    description: String;
+    url: String;
+}
+export const swipersInfo: SwiperInfo[] = [
+    {
+        title: " Leading Through Digital Innovation",
+        subTitle: "Rwanda Coding Academy",
+        description: "Fueling Rwanda's tech revolution! Open to all Rwandans post ordinary level. Elevate your prospects at the esteemed Rwanda Coding Academy—a beacon of excellence nationwide, nestled in Nyabihu District, Western Province ,Fueling Rwanda's tech revolution! Open to all Rwandans post ordinary level. Elevate your prospects at the esteemed Rwanda Coding Academy—a beacon of excellence nationwide, nestled in Nyabihu District, Western Province Fueling Rwanda's tech revolution! Open to all Rwandans post ordinary level. Elevate your prospects at the esteemed Rwanda Coding Academy—a beacon of excellence nationwide, nestled in Nyabihu District, Western Province Fueling Rwanda's tech revolution! Open to all Rwandans post ordinary level. Elevate your prospects at the esteemed Rwanda Coding Academy—a beacon of excellence nationwide, nestled in Nyabihu District, Western Province Fueling Rwanda's tech revolution! Open to all Rwandans post ordinary level. Elevate your prospects at the esteemed Rwanda Coding Academy—a beacon of excellence nationwide, nestled in Nyabihu District, Western Province",
+        url:"https://images.unsplash.com/photo-1568333261345-0918efdce2d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
+    }, {
+        title: " Leading Through Digital Innovation",
+        subTitle: "Rwanda Coding Academy",
+        description: "Fueling Rwanda's tech revolution! Open to all Rwandans post ordinary level. Elevate your prospects at the esteemed Rwanda Coding Academy—a beacon of excellence nationwide, nestled in Nyabihu District, Western Province",
+        url:"https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+    }
+    , {
+        title: " Leading Through Digital Innovation",
+        subTitle: "Rwanda Coding Academy",
+        description: "Fueling Rwanda's tech revolution! Open to all Rwandans post ordinary level. Elevate your prospects at the esteemed Rwanda Coding Academy—a beacon of excellence nationwide, nestled in Nyabihu District, Western Province",
+        url:"https://images.unsplash.com/photo-1544640808-32ca72ac7f37?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bGlicmFyeXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60"
+    }
+]

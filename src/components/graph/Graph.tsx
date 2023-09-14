584730a0a1639a793eaacfb49eb3563e69c72f0e
davidnyiringabo <nyiringabodavid62@gmail.com>
2023-09-14 17:29:39 +0200

    .

--- src/components/graph/Graph.tsx
+++ src/components/graph/Graph.tsx
@@ -1,81 +1,89 @@
-'use client'
-import React from 'react';
+"use client"
+import React from 'react'
 import { Line } from 'react-chartjs-2';
 import {
-  Chart as ChartJs,
-  ArcElement,
-  Tooltip,
-  LineElement,
-  CategoryScale,
-  Filler,
-  LinearScale,
-  Legend,
-  PointElement,
+        Chart as ChartJs,
+        ArcElement,
+        Tooltip,
+        LineElement,
+        CategoryScale,
+        Filler,
+        LinearScale,
+        Legend,
+        PointElement,
 } from "chart.js";
 
+
 ChartJs.register(
-  ArcElement,
-  Tooltip,
-  LineElement,
-  PointElement,
-  LinearScale,
-  Legend,
-  Filler,
-  CategoryScale
+        ArcElement,
+        Tooltip,
+        LineElement,
+        PointElement,
+        LinearScale,
+        Legend,
+        Filler,
+        CategoryScale
 );
-
 const Graph = () => {
-  const data = {
-    labels: ["", "", "", "", "", "", "", "", "", "", "", ""],
-    datasets: [
-      {
-        label: "",
-        data: [60, 80, 70, 89.4, 75, 80, 78, 76, 60, 80, 76, 90, 80],
-        tension: 0.1,
-        borderColor: "rgba(25, 188, 100, 0.63)",
-      },
-    ],
-  };
+        const data = {
+                labels: ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"],
+                datasets: [
+                        {
+                                label: "Mathematics",
+                                data: [40, 60, 20, 65, 35, 45, 95],
+                                tension: 0.4,
+                                borderColor: "#0500FF",
+                        },
+                        {
+                                label: "Mathematics",
+                                data: [80, 90, 40, 77, 45, 75, 55],
+                                tension: 0.4,
+                                borderColor: "#55BB8C",
+                        },
+                ],
+        };
+        const options = {
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                        legend: {
+                                display: false,
+                        },
+                },
+                layout: {
+                        padding: {
+                                left: 10,
+                                right: 10,
+                                top: 10,
+                                bottom: 10,
+                        },
+                },
+                scales: {
+                        x: {
+                                grid: {
+                                        display: false,
+                                },
+                        },
+                        y: {
+                                border: {
+                                        dash: [5],
+                                },
+                                grid: {
+                                        drawTicks: false,
+                                },
+                                ticks: {
+                                        stepSize: 10,
+                                        padding: 5,
+                                        callback: (value: any) => `${value}%`,
+                                },
+                        },
+                },
+        };
+        return (
+                <div className="h-[90%]">
+                        <Line data={data} options={options} />
+                </div>
+        )
+}
 
-  const options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {
-      legend: {
-        display: false,
-        }
-    },
-    scales: {
-      x: {
-        // display: false,
-        grid: {
-          display: false
-        },
-      },
-      y: {
-        // display: false,
-        border: {
-          dash: [5],
-        },
-        grid: {
-          drawTicks: false,
-        },
-        ticks: {
-          display: false, // Hide y-axis scale numbers
-        },
-      },
-    },
-  };
-
-  return (
-    <div className="w-full relative flex justify-center items-center">
-        <p className="absolute text-[rgba(0, 0, 0, 0.4)] left-[-2%] text-[12px] horizo-writing">Grades (%)</p>
-        <p className="absolute text-[rgba(0, 0, 0, 0.4)] bottom-[-2%] text-[12px] verti-writing">Academic terms</p>
-        <div className='w-[94%] h-[150px]'>
-                <Line data={data} options={options} />
-        </div>
-    </div>
-  );
-};
-
-export default Graph;
+export default Graph
